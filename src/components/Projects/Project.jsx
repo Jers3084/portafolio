a8bbbf19a0ace395629c4260684755f54a0d2e50
JESUS EUGENIO RICARDEZ <jericrdez.c03@ucamp.utel.edu.mx>
2022-04-02 07:32:36 -0600

    Update
    Update

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
-import styles from './Project.module.css'
-import proyectos from './Projects.json'
-
+import React from "react";
+import styles from "./Project.module.css";
+import proyectos from "./Projects.json";
 
 export const Project = () => {
   return (
     <div>
       <h1>Project</h1>
-          <div className={styles.contenido}>
-            {proyectos.map((x, index) => {
-              return (
-                <div key={index} className={styles.card}>
-                  <div className={styles.imagen_card}>
-                    <img src={x.imagen} alt="" style={{with:'150px', height:'200px'}} />
-                  </div>
-                  <div className={styles.title_card}>
-                    <h3>{x.nombre}</h3>
-                  </div>
-                  <div className={styles.descripcion_card}>
-                    <h3>{x.descripcion}</h3>
-                  </div>
-                  <a className={styles.descripcion_card} href={x.path} target="_blank"  rel="noopener noreferrer">Ejecutar
-                  </a>
-                </div>
-              )
-            })}
-          </div>
-
+      <div className={styles.contenido}>
+        {proyectos.map((x, index) => {
+          return (
+            <div key={index} className={styles.card}>
+              <div className={styles.imagen_card}>
+                <img
+                  src={x.imagen}
+                  alt=""
+                  style={{ with: "150px", height: "200px" }}
+                />
+              </div>
+              <div className={styles.title_card}>
+                <h3>{x.nombre}</h3>
+              </div>
+              <div className={styles.descripcion_card}>
+                <h3>{x.descripcion}</h3>
+              </div>
+              <a
+                className={styles.descripcion_card}
+                href={x.path}
+                target="_blank"
+                rel="noopener noreferrer">
+                Ejecutar
+              </a>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
